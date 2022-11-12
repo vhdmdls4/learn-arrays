f1499094894380f543344873730099b75ea37bff
Victor Hugo Martins Duarte Lima <vhduartelima@hotmail.com>
2022-11-11 22:25:08 -0300

    sec phase

--- att-arrays.js
+++ att-arrays.js
@@ -1,3 +1,14 @@
-const nomes = ["joao","ana","caio","lara","marjorie","leo"];
+const nomes = ["joao", "ana", "caio", "lara", "marjorie", "leo"]
 //primeiro arg. = indice do primeiro que quer remover e o segundo a quantidade de itens que removerá a partir daí;
-nomes.splice(1,3);
+nomes.splice(1, 3, "rodrigo")
+
+console.log(nomes)
+
+nomes.push("Rodrigo")
+
+animaisDoAquario = ["🐋 1", "🐙 2", "🐬 3", "🦈 4"]
+
+animaisDoAquario.splice(1, 3, "🐠 5")
+// animaisDoAquario.splice(3, 2, "🐟 6")
+
+console.log(animaisDoAquario)
